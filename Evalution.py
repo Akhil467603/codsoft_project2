b19ea5ee2e815f86529384f0fa91d8315bf18191
Akhil467603 <akhilmushke4676@gmail.com>
2024-08-14 22:08:59 +0530

    Create Evalution.py

---
+++ Evalution.py
@@ -0,0 +1,7 @@
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Root Mean Squared Error: {rmse}")
+print(f"R-squared: {r2}")
